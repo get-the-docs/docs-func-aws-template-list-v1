3b9a07af46b840d771374f9867ffd7c03d843134
videki <videki@users.noreply.github.com>
2023-03-22 13:05:34 +0100

    feat: story/GD-40 unit tests added

---
+++ tests/unit/handler-invalid.test.ts
@@ -0,0 +1,105 @@
+'use strict';
+
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context
+} from "aws-lambda";
+import { lambdaHandler } from "../../src/handler";
+import {GetTemplatesResponse} from "../../src/gen/model/getTemplatesResponse";
+
+describe('Lambda handler tests - negative tests', function () {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules()
+    process.env = { ...OLD_ENV };
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV;
+  });
+
+  it('Param error - Invalid bucket name', async () => {
+
+    process.env["repository_template_provider_aws_s3_region"] = "us-east-2";
+    process.env["repository_template_provider_aws_s3_bucketname"] = "";
+    process.env["repository_template_provider_aws_s3_prefix"] = "";
+
+    const event: APIGatewayProxyEvent = {
+    } as any;
+
+    const testContext: Partial<Context> = {
+      awsRequestId: "cd4b63fd-e407-450d-b24e-1e991ea43425",
+      logGroupName:"/aws/lambda/docs-func-aws-template-fill-v1",
+      logStreamName: "2023/03/01/[$LATEST]12345abcdfe01234567890abcdef1234",
+      functionName: "docs-func-aws-template-fill-v1",
+      functionVersion: "$LATEST",
+      invokedFunctionArn: "arn:aws:lambda:us-east-2:123456789012:function:docs-func-aws-template-fill-v1",
+      identity: undefined,
+      clientContext: undefined,
+      memoryLimitInMB: "512"
+    };
+
+    await expect(
+      lambdaHandler(event as APIGatewayProxyEvent, testContext as Context)
+    ).rejects.toThrow("Bucket name or region not specified.");
+  });
+
+  it('Param error - Invalid prefix', async () => {
+
+    process.env["repository_template_provider_aws_s3_region"] = OLD_ENV["repository_template_provider_aws_s3_region"];
+    process.env["repository_template_provider_aws_s3_bucketname"] = OLD_ENV["repository_template_provider_aws_s3_bucketname"];
+    process.env["repository_template_provider_aws_s3_prefix"] = "nonexisting_folder";
+
+    const event: APIGatewayProxyEvent = {
+    } as any;
+
+    const testContext: Partial<Context> = {
+      awsRequestId: "cd4b63fd-e407-450d-b24e-1e991ea43425",
+      logGroupName:"/aws/lambda/docs-func-aws-template-fill-v1",
+      logStreamName: "2023/03/01/[$LATEST]12345abcdfe01234567890abcdef1234",
+      functionName: "docs-func-aws-template-fill-v1",
+      functionVersion: "$LATEST",
+      invokedFunctionArn: "arn:aws:lambda:us-east-2:123456789012:function:docs-func-aws-template-fill-v1",
+      identity: undefined,
+      clientContext: undefined,
+      memoryLimitInMB: "512"
+    };
+
+    const result: APIGatewayProxyResult =
+      await lambdaHandler(event as APIGatewayProxyEvent, testContext as Context);
+
+    expect(result.statusCode).toEqual(404);
+
+    const resultObjects: GetTemplatesResponse = JSON.parse(result.body);
+    expect(resultObjects.contents!.length).toBe(0);
+  });
+
+  it('Param error - Wrong region', async () => {
+
+    process.env["repository_template_provider_aws_s3_region"] = "us-east-2";
+    process.env["repository_template_provider_aws_s3_bucketname"] = OLD_ENV["repository_template_provider_aws_s3_bucketname"];
+    process.env["repository_template_provider_aws_s3_prefix"] = OLD_ENV["repository_template_provider_aws_s3_prefix"];
+
+    const event: APIGatewayProxyEvent = {
+    } as any;
+
+    const testContext: Partial<Context> = {
+      awsRequestId: "cd4b63fd-e407-450d-b24e-1e991ea43425",
+      logGroupName:"/aws/lambda/docs-func-aws-template-fill-v1",
+      logStreamName: "2023/03/01/[$LATEST]12345abcdfe01234567890abcdef1234",
+      functionName: "docs-func-aws-template-fill-v1",
+      functionVersion: "$LATEST",
+      invokedFunctionArn: "arn:aws:lambda:us-east-2:123456789012:function:docs-func-aws-template-fill-v1",
+      identity: undefined,
+      clientContext: undefined,
+      memoryLimitInMB: "512"
+    };
+
+    await expect(
+      lambdaHandler(event as APIGatewayProxyEvent, testContext as Context)
+    ).rejects.toThrow("Error retrieving the object list from S3.");
+  });
+
+});
